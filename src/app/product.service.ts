6c9cd2c2f46016fc7a274fea7263290eca9464bd
Manjunath, Sunil <sunil.manjunath@tesco.com>
2018-12-03 18:41:18 +0530

    8

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
+//import { Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) 
+  
+  { 
+
+    
+  }
+
+  getAlbum(id: number) {
+
+    return this._http.get(this._albumUrl).map((response) =>
+    response.json());
+
+  }
 
 }
